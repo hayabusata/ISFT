1a6201282bdd0d569ed9bfab987dc37634c90fac
Shion <f.shion1118@gmail.com>
2017-02-25 21:59:30 +0900

    change: TabHost to FragmentTabHost

---
+++ app/src/main/java/falleight/isft/OccupancyStatusActivity2.java
@@ -0,0 +1,37 @@
+package falleight.isft;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OccupancyStatusActivity2 extends Fragment {
+
+    public OccupancyStatusActivity2() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.occupation_status, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
