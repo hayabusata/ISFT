9467a388e4aa1bcf86793cf3169ed4eb130f937a
hayabusata <falcon.cryptomeria@gmail.com>
2017-02-27 15:30:02 +0900

    メール送信機能追加

--- app/src/main/java/falleight/isft/TeacherProfileActivity.java
+++ app/src/main/java/falleight/isft/TeacherProfileActivity.java
@@ -1,21 +1,23 @@
 package falleight.isft;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class TeacherProfileActivity extends AppCompatActivity {
+public class TeacherProfileActivity extends AppCompatActivity implements View.OnClickListener{
     TextView nameView, statusView, emailView, roomNumberView, wordView;
     String name, status, email, roomNumber, word;
-    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teacher_profile);
 
-        intent = getIntent();
+        Intent intent = getIntent();
         if (intent != null) {
             name = intent.getStringExtra("name");
             roomNumber = intent.getStringExtra("roomnumber");
@@ -30,10 +32,31 @@ public class TeacherProfileActivity extends AppCompatActivity {
         roomNumberView = (TextView)findViewById(R.id.RoomNumberView);
         wordView = (TextView)findViewById(R.id.WordView);
 
+        ((Button)findViewById(R.id.sendEmail)).setOnClickListener(this);
+
         nameView.setText(name);
         statusView.setText(status);
         emailView.setText(email);
         roomNumberView.setText(roomNumber);
         wordView.setText(word);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sendEmail:
+                Intent emailIntent = new Intent();
+                emailIntent.setAction(Intent.ACTION_SENDTO);
+
+                emailIntent.setType("text/plain");
+                emailIntent.setData(Uri.parse("mail:to" + email));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "タイトル");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "本文");
+
+                startActivity(Intent.createChooser(emailIntent, null));
+                break;
+            default:
+                break;
+        }
     }
 }
